6ea5ca0f10ec8d0892c8bf808830d871784fe0ea
Simone <simone.abelli@mail.polimi.it>
2021-07-09 18:15:21 +0200

    Under the spotlight

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -4,6 +4,7 @@ precision mediump float;
 
 in vec3 fs_norm;
 in vec3 fs_pos;
+in vec3 fs_camera_pos;
 in vec2 fs_uv;
 
 out vec4 outColor;
@@ -17,8 +18,10 @@ uniform vec3 directionalLightColor; 				      //directional light color
 uniform vec3 spotLightDirection; 			    //Spot light direction vec
 uniform vec3 spotLightColor; 				      //Spot light color
 uniform vec3 spotLightPosition; 			    //Spot light direction vec
-uniform float coneIn; 				            //Spot inner cone
-uniform float coneOut; 				            //Spot outer cone
+uniform float coneIn; 				            //Spot inner cone (in percentage of the outher cone)
+uniform float coneOut; 				            //Spot outer cone (in degree)
+uniform float target;
+uniform float decay;
 
 uniform vec3 effects; 				        //select the light effect
 uniform mat4 inverseViewProjMatrix; 	//inv-transp of the ViewProjMatrix
@@ -27,7 +30,7 @@ uniform sampler2D sampler;
 
 void main() {
 
-  vec4 p = vec4(fs_pos, 1.0);
+  vec4 p = vec4(fs_camera_pos, 1.0);
 
   vec4 cubemapRgba = texture(cubemap, normalize(p.xyz / p.w));
 
@@ -39,15 +42,17 @@ void main() {
   vec3 directionalLambert = mDiffColor * directionalLightColor * dot(-lightDirNorm, nNormal);
 
   vec3 spotLightDir = normalize(spotLightPosition - fs_pos);
+  
   float CosAngle = dot(spotLightDir, spotLightDirection);
   float LCosOut = cos(radians(coneOut / 2.0));
 	float LCosIn = cos(radians(coneOut * coneIn / 2.0));
-	vec4 spotLightCol = vec4(spotLightColor, 1.0) * clamp((CosAngle - LCosOut) / (LCosIn - LCosOut), 0.0, 1.0);
+	vec4 spotLightCol = vec4(spotLightColor, 1.0) * pow(target / length(spotLightPosition - fs_pos), decay)
+                                                * clamp((CosAngle - LCosOut) / (LCosIn - LCosOut), 0.0, 1.0);
 
   float LdotN = max(0.0, dot(nNormal, spotLightDir));
 	vec4 LDcol = spotLightCol * vec4(mDiffColor, 1.0);
 	vec4 spotLambert = LDcol * LdotN;
 
   float skybox = effects.r;
-  outColor = mix((vec4(spotLambert.rgb, 1.0)), cubemapRgba, skybox);
+  outColor = mix((vec4(spotLambert.rgb, 1.0) + vec4(directionalLambert.rgb, 1.0)), cubemapRgba, skybox);
 } 
--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -6,15 +6,17 @@ in vec2 in_uv;
 
 out vec3 fs_norm;
 out vec3 fs_pos;
+out vec3 fs_camera_pos;
 out vec2 fs_uv;
 
 uniform mat4 wvpMatrix; 	//WVP martix
-uniform mat4 pMatrix;
+uniform mat4 pMatrix;		//WV matrix
 uniform mat4 nMatrix; 		//InvTransp of the World-View matrix
 
 void main() {
 	gl_Position = wvpMatrix * vec4(in_pos, 1.0);
 	fs_norm = mat3(nMatrix) * in_norm;
-	fs_pos = in_pos;
+	fs_pos = (pMatrix * vec4(in_pos, 1.0)).xyz;
+	fs_camera_pos = in_pos;
 	fs_uv = in_uv;
 }
